f5f05be048db5164005509671a4862acb08222ce
dimotim <datdimotim@gmail.com>
2022-07-23 22:45:06 +0300

    limit range filter range moved to application.yml

--- backend/src/main/java/com/dimotim/spring/content/app/config/ContentFilter.java
+++ backend/src/main/java/com/dimotim/spring/content/app/config/ContentFilter.java
@@ -1,23 +1,30 @@
+/* (C)2022 */
 package com.dimotim.spring.content.app.config;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
-
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Enumeration;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
 
 @Component
 @WebFilter(urlPatterns = "/api/files/*")
 public class ContentFilter implements Filter {
-    public static final int MAX_CONTENT_LENGTH = 1024 * 1024 * 2;
     private final Pattern pattern = Pattern.compile("^bytes=(?<start>\\d+)-$");
+    private final int maxRange;
+
+    @Autowired
+    public ContentFilter(@Value("${application.max-range-size-kb}") int maxRange) {
+        this.maxRange = maxRange * 1034;
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -41,45 +48,44 @@ public class ContentFilter implements Filter {
         }
 
         int start = Integer.parseInt(matcher.group("start"));
-        int end = start + MAX_CONTENT_LENGTH;
+        int end = start + maxRange;
 
         String header = "bytes=" + start + "-" + end;
 
-        HttpServletRequestWrapper wrapper = new HttpServletRequestWrapper(req) {
-            @Override
-            public String getHeader(String name) {
-                if (HttpHeaders.RANGE.equalsIgnoreCase(name)) {
-                    return header;
-                } else {
-                    return super.getHeader(name);
-                }
-            }
-
-            @Override
-            public Enumeration<String> getHeaders(String name) {
-                if (HttpHeaders.RANGE.equalsIgnoreCase(name)) {
-                    return new Enumeration<>() {
-                        boolean hasMore = true;
-
-                        @Override
-                        public boolean hasMoreElements() {
-                            return hasMore;
-                        }
-
-                        @Override
-                        public String nextElement() {
-                            hasMore = false;
+        HttpServletRequestWrapper wrapper =
+                new HttpServletRequestWrapper(req) {
+                    @Override
+                    public String getHeader(String name) {
+                        if (HttpHeaders.RANGE.equalsIgnoreCase(name)) {
                             return header;
+                        } else {
+                            return super.getHeader(name);
                         }
-                    };
-                } else {
-                    return super.getHeaders(name);
-                }
-            }
-        };
+                    }
 
+                    @Override
+                    public Enumeration<String> getHeaders(String name) {
+                        if (HttpHeaders.RANGE.equalsIgnoreCase(name)) {
+                            return new Enumeration<>() {
+                                boolean hasMore = true;
+
+                                @Override
+                                public boolean hasMoreElements() {
+                                    return hasMore;
+                                }
+
+                                @Override
+                                public String nextElement() {
+                                    hasMore = false;
+                                    return header;
+                                }
+                            };
+                        } else {
+                            return super.getHeaders(name);
+                        }
+                    }
+                };
 
         chain.doFilter(wrapper, response);
     }
-
 }
