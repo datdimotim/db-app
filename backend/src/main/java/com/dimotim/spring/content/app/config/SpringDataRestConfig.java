d390d09fce775677709d66e30289aa7cbcee7306
dimotim <datdimotim@gmail.com>
2022-08-12 00:06:00 +0300

    readmed to SpringDataRestConfig

--- backend/src/main/java/com/dimotim/spring/content/app/config/SpringDataRestConfig.java
+++ backend/src/main/java/com/dimotim/spring/content/app/config/SpringDataRestConfig.java
@@ -10,7 +10,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class Config implements RepositoryRestConfigurer {
+public class SpringDataRestConfig implements RepositoryRestConfigurer {
 
     @Autowired private EntityManager entityManager;
 
